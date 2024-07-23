6280229bcc0a2106b846b9e352d7fe5e331cf72b
MrNiNaD <ninadparkar273@gmail.com>
2024-07-24 00:13:19 +0530

    feat: accordion component added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import { AccordionDemo } from "@/customcomponent/Accordion";
 import "./style.css";
 
 export default function Home() {
-  return <p>Travel App(Deployment Test22222)</p>;
+  return (
+    <>
+      <p>Travel App(Deployment Test22222)</p>
+      <AccordionDemo />
+    </>
+  );
 }
