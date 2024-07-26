f0a108553786aef7674c5576c32b3abfd759466c
MrNiNaD <ninadparkar273@gmail.com>
2024-07-27 00:52:37 +0530

    feat: search bar added in topbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,3 @@
-import { AccordionDemo } from "@/customcomponent/Accordion";
-import "./style.css";
-import { Input } from "@/components/ui/input";
-
 export default function Home() {
-  return (
-    <>
-      <p className="text-primary-color">Travel App(Deployment Test22222)</p>
-      <AccordionDemo />
-      <Input />
-    </>
-  );
+  return <></>;
 }
---
+++ src/customcomponent/TopBar/index.tsx
@@ -0,0 +1,39 @@
+import Link from "next/link";
+import React from "react";
+
+const TopBar = () => {
+  return (
+    <header className="bg-primary-color py-[8px]">
+      <div className="wrapper flex justify-between items-center">
+        <h1 className="text-white uppercase font-bold">
+          <Link href="/">Travel App</Link>
+        </h1>
+
+        <div className="w-[40%] relative">
+          <svg
+            width="15"
+            height="15"
+            viewBox="0 0 15 15"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="absolute top-[50%] left-[10px] translate-y-[-50%]"
+          >
+            <g id="Basics / search">
+              <path
+                id="Vector"
+                d="M14.05 14.05L10.45 10.45M6.84995 12.25C3.86761 12.25 1.44995 9.83229 1.44995 6.84995C1.44995 3.86761 3.86761 1.44995 6.84995 1.44995C9.83229 1.44995 12.25 3.86761 12.25 6.84995C12.25 9.83229 9.83229 12.25 6.84995 12.25Z"
+                stroke="#999999"
+              />
+            </g>
+          </svg>
+          <input
+            placeholder="Search"
+            className="w-full focus:outline-none pr-[15px] pl-[32px] py-[7px] rounded-[999px] text-[13px] placeholder:text-[12px]"
+          />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default TopBar;
